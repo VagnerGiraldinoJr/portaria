f8c8f5490cf9f5db7ebd2df757d0707a80c114fd
Vagner <vgiraldinojr@gmail.com>
2024-06-21 17:04:06 -0300

    Ordenação na tabela ControleAcesso.

--- app/Http/Controllers/Admin/ControleAcessoController.php
+++ app/Http/Controllers/Admin/ControleAcessoController.php
@@ -16,160 +16,165 @@ class ControleAcessoController extends Controller
 {
     private $params = [];
     private $controle_acesso = [];
-    private $lote ; 
+    private $lote;
     public function __construct(ControleAcesso $controle_acessos, Lote $lotes)
-    { 
+    {
         $this->controle_acesso = $controle_acessos;
         $this->lote = $lotes;
         // Default values
-        $this->params['titulo']='Controle de Acesso da Portaria';
-        $this->params['main_route']='admin.controleacesso';
-        }
+        $this->params['titulo'] = 'Controle de Acesso da Portaria';
+        $this->params['main_route'] = 'admin.controleacesso';
+    }
 
     public function index()
     {
         // PARAMS DEFAULT
-        $this->params['subtitulo']='Controle de Acesso da Portaria';
+        $this->params['subtitulo'] = 'Controle de Acesso da Portaria';
         $this->params['arvore'][0] = [
-                    'url' => 'admin/controleacesso',
-                    'titulo' => 'Controle de Acessos'
+            'url' => 'admin/controleacesso',
+            'titulo' => 'Controle de Acessos'
         ];
         $params = $this->params;
         $data = $this->controle_acesso
-     
-        ->where('unidade_id', Auth::user()->unidade_id)
-        ->with('lote')
-        ->with('veiculo')
-        ->orderBy('data_entrada','desc') // Ordenar por data_inicio em ordem decrescente
-        ->get();
-        return view('admin.controleacesso.index',compact('params','data'));
+
+            ->where('unidade_id', Auth::user()->unidade_id)
+            ->with('lote')
+            ->with('veiculo')
+            ->orderByRaw('data_saida IS NULL DESC')
+            ->get();
+
+        return view('admin.controleacesso.index', compact('params', 'data'));
     }
     public function create(TableCode $codes)
     {
         // PARAMS DEFAULT
-        $this->params['subtitulo']='Cadastrar Controle de Acessos';
-        $this->params['arvore']=[
-        [
-        'url' => 'admin/controleacesso',
-        'titulo' => 'Controle de Acesso'
-        ],
-        [
-            'url' => '',
-            'titulo' => 'Cadastrar'
-            ]];
-   
-    // preload          
-    $preload['tipo'] = $codes->select(5);
-    
-    // Buscar os lotes (unidades) ordenados por descrição em ordem ascendente
-    $preload['unidade'] = Lote::where('unidade_id', Auth::user()->unidade_id)
-                    ->orderBy('id', 'asc') // Ordenar por descrição em ordem ascendente
-                    ->pluck('descricao', 'id');
+        $this->params['subtitulo'] = 'Cadastrar Controle de Acessos';
+        $this->params['arvore'] = [
+            [
+                'url' => 'admin/controleacesso',
+                'titulo' => 'Controle de Acesso'
+            ],
+            [
+                'url' => '',
+                'titulo' => 'Cadastrar'
+            ]
+        ];
 
-    $params = $this->params;  
-                
-    return view('admin.controleacesso.create',compact('params','preload'));
+        // preload          
+        $preload['tipo'] = $codes->select(5);
+
+        // Buscar os lotes (unidades) ordenados por descrição em ordem ascendente
+        $preload['unidade'] = Lote::where('unidade_id', Auth::user()->unidade_id)
+            ->orderBy('id', 'asc') // Ordenar por descrição em ordem ascendente
+            ->pluck('descricao', 'id');
+
+        $params = $this->params;
+
+        return view('admin.controleacesso.create', compact('params', 'preload'));
     }
- 
+
     public function store(ControleAcessoRequest $request)
     {
         $dataForm  = $request->all();
         // id, unidade_id, tipo, lote_id, veiculo_id, motorista, motivo, observacao, data_entrada, data_saida, created_at, updated_at
-       
+
         $dataForm['data_entrada'] = Carbon::now()->format('Y-m-d H:i:s');
         $dataForm['unidade_id'] = Auth::user()->unidade_id;
         $insert = $this->controle_acesso->create($dataForm);
-        if($insert){
-            return redirect()->route($this->params['main_route'].'.index');
-        }else{
-            return redirect()->route($this->params['main_route'].'.create')->withErrors(['Falha ao fazer Inserir.']);
-        } 
+        if ($insert) {
+            return redirect()->route($this->params['main_route'] . '.index');
+        } else {
+            return redirect()->route($this->params['main_route'] . '.create')->withErrors(['Falha ao fazer Inserir.']);
+        }
     }
     public function show($id, TableCode $codes)
     {
-        $this->params['subtitulo']='Atualizar Controle de Acesso';
-        $this->params['arvore']=[
-           [
-               'url' => 'admin/controle_acessos',
-               'titulo' => 'Controle de Acessos'
-           ],
-           [
-               'url' => '',
-               'titulo' => 'Editar'
-           ]];
-       $params = $this->params;
-       $data = $this->controle_acesso->with('pessoa')->with('veiculo')->find($id);
-       $data = $this->controle_acesso->where('unidade_id',Auth::user()->unidade_id)->where('id',$id)->first();
-       $preload['tipo'] = $codes->select(5);
-            
+        $this->params['subtitulo'] = 'Atualizar Controle de Acesso';
+        $this->params['arvore'] = [
+            [
+                'url' => 'admin/controle_acessos',
+                'titulo' => 'Controle de Acessos'
+            ],
+            [
+                'url' => '',
+                'titulo' => 'Editar'
+            ]
+        ];
+        $params = $this->params;
+        $data = $this->controle_acesso->with('pessoa')->with('veiculo')->find($id);
+        $data = $this->controle_acesso->where('unidade_id', Auth::user()->unidade_id)->where('id', $id)->first();
+        $preload['tipo'] = $codes->select(5);
 
-       return view('admin.controleacesso.show',compact('params', 'data','preload'));
+
+        return view('admin.controleacesso.show', compact('params', 'data', 'preload'));
     }
 
     public function edit($id, TableCode $codes)
     {
-        $this->params['subtitulo']='Editar Controle de Acesso';
-        $this->params['arvore']=[
-           [
-               'url' => 'admin/controleacesso',
-               'titulo' => 'Controles'
-           ],
-           [
-               'url' => '',
-               'titulo' => 'Editar'
-           ]];
-       $params = $this->params;
-       
-       $data = $this->controle_acesso->with('pessoa')->with('veiculo')->find($id);
-            
-       $preload['tipo'] = $codes->select(5);
-       return view('admin.controleacesso.create',compact('params', 'data','preload'));
+        $this->params['subtitulo'] = 'Editar Controle de Acesso';
+        $this->params['arvore'] = [
+            [
+                'url' => 'admin/controleacesso',
+                'titulo' => 'Controles'
+            ],
+            [
+                'url' => '',
+                'titulo' => 'Editar'
+            ]
+        ];
+        $params = $this->params;
+
+        $data = $this->controle_acesso->with('pessoa')->with('veiculo')->find($id);
+
+        $preload['tipo'] = $codes->select(5);
+        return view('admin.controleacesso.create', compact('params', 'data', 'preload'));
     }
 
     public function exit($id, TableCode $codes)
     {
-        $this->params['subtitulo']='Editar Controle de Acesso';
-        $this->params['arvore']=[
-           [
-               'url' => 'admin/controleacesso',
-               'titulo' => 'Controles'
-           ],
-           [
-               'url' => '',
-               'titulo' => 'Editar'
-           ]];
-       $params = $this->params;
-       
-       $data = $this->controle_acesso->find($id);
+        $this->params['subtitulo'] = 'Editar Controle de Acesso';
+        $this->params['arvore'] = [
+            [
+                'url' => 'admin/controleacesso',
+                'titulo' => 'Controles'
+            ],
+            [
+                'url' => '',
+                'titulo' => 'Editar'
+            ]
+        ];
+        $params = $this->params;
 
-       $preload['tipo'] = $codes->select(5);
-       return view('admin.controleacesso.exit',compact('params', 'data','preload'));
+        $data = $this->controle_acesso->find($id);
+
+        $preload['tipo'] = $codes->select(5);
+        return view('admin.controleacesso.exit', compact('params', 'data', 'preload'));
     }
 
     public function update(Request $request, $id)
     {
         $dataForm  = $request->all();
-       //Pull
-        $dataForm['data_entrada'] = Carbon::parse($dataForm['data_entrada'])->format('Y-m-d H:i:s'); 
-       
-        if($this->controle_acesso->find($id)->update($dataForm)){
-            return redirect()->route($this->params['main_route'].'.index');
-        }else{
-            return redirect()->route($this->params['main_route'].'.create')->withErrors(['Falha ao editar.']);
+        //Pull
+        $dataForm['data_entrada'] = Carbon::parse($dataForm['data_entrada'])->format('Y-m-d H:i:s');
+
+        if ($this->controle_acesso->find($id)->update($dataForm)) {
+            return redirect()->route($this->params['main_route'] . '.index');
+        } else {
+            return redirect()->route($this->params['main_route'] . '.create')->withErrors(['Falha ao editar.']);
         }
     }
 
-   
+
     public function updateexit(Request $request, $id)
     {
-        $dataForm  = $request->only('data_saida','retirado_por');
-        
-        $dataForm['data_saida'] = Carbon::parse($dataForm['data_saida'])->format('Y-m-d H:i:s'); 
+        $dataForm  = $request->only('data_saida', 'retirado_por');
 
-        if($this->controle_acesso->find($id)->update($dataForm)){
-            return redirect()->route($this->params['main_route'].'.index');
-        }else{
-            return redirect()->route($this->params['main_route'].'.create')->withErrors(['Falha ao editar.']);
+        $dataForm['data_saida'] = Carbon::parse($dataForm['data_saida'])->format('Y-m-d H:i:s');
+
+        if ($this->controle_acesso->find($id)->update($dataForm)) {
+            return redirect()->route($this->params['main_route'] . '.index');
+        } else {
+            return redirect()->route($this->params['main_route'] . '.create')->withErrors(['Falha ao editar.']);
         }
     }
 
@@ -177,10 +182,10 @@ class ControleAcessoController extends Controller
     {
         $data = $this->controle_acesso->find($id);
 
-        if($data->delete()){
-            return redirect()->route($this->params['main_route'].'.index');
-        }else{
-            return redirect()->route($this->params['main_route'].'.create')->withErrors(['Falha ao deletar.']);
+        if ($data->delete()) {
+            return redirect()->route($this->params['main_route'] . '.index');
+        } else {
+            return redirect()->route($this->params['main_route'] . '.create')->withErrors(['Falha ao deletar.']);
         }
     }
 
@@ -188,10 +193,4 @@ class ControleAcessoController extends Controller
     {
         $data  = $this->controle_acesso('data_saida');
     }
-
-   
-
-
-   
-
 }
