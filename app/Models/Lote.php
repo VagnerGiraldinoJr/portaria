adae61d34bee041553ff1a93e8bcb981900846d1
Vagner <vgiraldinojr@gmail.com>
2024-07-25 10:00:51 -0300

    Lote Model Relação

--- app/Models/Lote.php
+++ app/Models/Lote.php
@@ -6,13 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Lote extends Model
 {
-    protected $fillable = ["unidade_id","descricao"];
+    protected $fillable = ["unidade_id", "descricao"];
 
     // Um Lote pode ter muitas Reservas
     public function reservas()
     {
-        return $this->hasMany(Reserva::class, 'lote_id'); 
+        return $this->hasMany(Reserva::class, 'lote_id');
     }
-   
-}
 
+    // Em App\Models\Lote
+    public function unidade()
+    {
+        return $this->belongsTo(Unidade::class);
+    }
+}
