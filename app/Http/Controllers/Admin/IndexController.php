707453ab359abda8f8eb603e98ab95d1e9b60f01
Vagner <vgiraldinojr@gmail.com>
2024-07-29 13:20:21 -0300

    WhereNotNull

--- app/Http/Controllers/Admin/IndexController.php
+++ app/Http/Controllers/Admin/IndexController.php
@@ -76,12 +76,12 @@ class IndexController extends Controller
         $data['QuantidadesCadVisitantes'] = $this->visitante->where('unidade_id', $unidadeId)->count();
         $data['QuantidadesReservas'] = $this->reserva->where('unidade_id', $unidadeId)->whereNull('dt_entrega_chaves')->count();
         $data['QuantidadesReservasPorMes'] = DB::table('reservas')
-                ->select(DB::raw("DATE_FORMAT(created_at, '%Y-%m') as mes"), DB::raw('COUNT(*) as total_reservas'))
-                ->where('unidade_id', $unidadeId)
-                ->whereNull('dt_entrega_chaves')
-                ->groupBy('mes')
-                ->orderBy('mes')
-                ->get();
+        ->select(DB::raw("DATE_FORMAT(created_at, '%Y-%m') as mes"), DB::raw('COUNT(*) as total_reservas'))
+        ->where('unidade_id', $unidadeId)
+        ->whereNotNull('data_inicio') // 'data_inicio' não é nulo
+        ->groupBy('mes')
+        ->orderBy('mes')
+        ->get();
          
 
 
